cab66393d7be5bedfb1dd493fe0775be8fc76370
koitt04 <koitt04@koitt04-PC>
2020-03-18 18:18:34 +0900

    0318 back 작업

---
+++ src/main/java/com/data/vo/MovieVO.java
@@ -0,0 +1,292 @@
+package com.data.vo;
+
+public class MovieVO {
+	private String movieCd,				// 영화코드를 출력합니다.
+				   movieNm,				// 영화명(국문)을 출력합니다.
+				   movieNmEn,			// 영화명(영문)을 출력합니다.
+				   movieNmOg,			// 영화명(원문)을 출력합니다.
+				   prdtYear,			// 제작연도를 출력합니다.
+				   showTm,				// 상영시간을 출력합니다.
+				   openDt,				// 개봉연도를 출력합니다.
+				   prdtStatNm,			// 제작상태명을 출력합니다.
+				   typeNm,				// 영화유형명을 출력합니다.
+				   nations,				// 제작국가를 나타냅니다.
+				   nationNm,			// 제작국가명을 출력합니다.
+				   genreNm,				// 장르명을 출력합니다.
+				   directors_peopleNm,				// 감독명을 출력합니다.
+				   directors_peopleNmEn,				// 감독명(영문)을 출력합니다.
+				   actors_peopleNm,		// 배우명을 출력합니다.
+				   actors_peopleNmEn,	// 배우명(영문)을 출력합니다.
+				   cast,				// 배역명을 출력합니다.
+				   castEn,				// 배역명(영문)을 출력합니다.
+				   showTypes,			// 상영형태 구분을 나타냅니다.
+				   showTypeGroupNm,		// 상영형태 구분을 출력합니다.
+				   showTypeNm,			// 상영형태명을 출력합니다.
+				   audits,				// 심의정보를 나타냅니다.
+				   auditNo,				// 심의번호를 출력합니다.
+				   watchGradeNm,		// 관람등급 명칭을 출력합니다.
+				   companys,			// 참여 영화사를 나타냅니다.
+				   companyCd,			// 참여 영화사 코드를 출력합니다.
+				   companyNm,			// 참여 영화사명을 출력합니다.
+				   companyNmEn,			// 참여 영화사명(영문)을 출력합니다.
+				   companyPartNm,		// 참여 영화사 분야명을 출력합니다.
+				   staffs_peopleNm,		// 스텝명을 출력합니다.
+				   staffs_peopleNmEn,	// 스텝명(영문)을 출력합니다.
+				   staffRoleNm;			// 스텝역할명을 출력합니다.
+
+	public String getMovieCd() {
+		return movieCd;
+	}
+
+	public void setMovieCd(String movieCd) {
+		this.movieCd = movieCd;
+	}
+
+	public String getMovieNm() {
+		return movieNm;
+	}
+
+	public void setMovieNm(String movieNm) {
+		this.movieNm = movieNm;
+	}
+
+	public String getMovieNmEn() {
+		return movieNmEn;
+	}
+
+	public void setMovieNmEn(String movieNmEn) {
+		this.movieNmEn = movieNmEn;
+	}
+
+	public String getMovieNmOg() {
+		return movieNmOg;
+	}
+
+	public void setMovieNmOg(String movieNmOg) {
+		this.movieNmOg = movieNmOg;
+	}
+
+	public String getPrdtYear() {
+		return prdtYear;
+	}
+
+	public void setPrdtYear(String prdtYear) {
+		this.prdtYear = prdtYear;
+	}
+
+	public String getShowTm() {
+		return showTm;
+	}
+
+	public void setShowTm(String showTm) {
+		this.showTm = showTm;
+	}
+
+	public String getOpenDt() {
+		return openDt;
+	}
+
+	public void setOpenDt(String openDt) {
+		this.openDt = openDt;
+	}
+
+	public String getPrdtStatNm() {
+		return prdtStatNm;
+	}
+
+	public void setPrdtStatNm(String prdtStatNm) {
+		this.prdtStatNm = prdtStatNm;
+	}
+
+	public String getTypeNm() {
+		return typeNm;
+	}
+
+	public void setTypeNm(String typeNm) {
+		this.typeNm = typeNm;
+	}
+
+	public String getNations() {
+		return nations;
+	}
+
+	public void setNations(String nations) {
+		this.nations = nations;
+	}
+
+	public String getNationNm() {
+		return nationNm;
+	}
+
+	public void setNationNm(String nationNm) {
+		this.nationNm = nationNm;
+	}
+
+	public String getGenreNm() {
+		return genreNm;
+	}
+
+	public void setGenreNm(String genreNm) {
+		this.genreNm = genreNm;
+	}
+
+	public String getDirectors_peopleNm() {
+		return directors_peopleNm;
+	}
+
+	public void setDirectors_peopleNm(String directors_peopleNm) {
+		this.directors_peopleNm = directors_peopleNm;
+	}
+
+	public String getDirectors_peopleNmEn() {
+		return directors_peopleNmEn;
+	}
+
+	public void setDirectors_peopleNmEn(String directors_peopleNmEn) {
+		this.directors_peopleNmEn = directors_peopleNmEn;
+	}
+
+	public String getActors_peopleNm() {
+		return actors_peopleNm;
+	}
+
+	public void setActors_peopleNm(String actors_peopleNm) {
+		this.actors_peopleNm = actors_peopleNm;
+	}
+
+	public String getActors_peopleNmEn() {
+		return actors_peopleNmEn;
+	}
+
+	public void setActors_peopleNmEn(String actors_peopleNmEn) {
+		this.actors_peopleNmEn = actors_peopleNmEn;
+	}
+
+	public String getCast() {
+		return cast;
+	}
+
+	public void setCast(String cast) {
+		this.cast = cast;
+	}
+
+	public String getCastEn() {
+		return castEn;
+	}
+
+	public void setCastEn(String castEn) {
+		this.castEn = castEn;
+	}
+
+	public String getShowTypes() {
+		return showTypes;
+	}
+
+	public void setShowTypes(String showTypes) {
+		this.showTypes = showTypes;
+	}
+
+	public String getShowTypeGroupNm() {
+		return showTypeGroupNm;
+	}
+
+	public void setShowTypeGroupNm(String showTypeGroupNm) {
+		this.showTypeGroupNm = showTypeGroupNm;
+	}
+
+	public String getShowTypeNm() {
+		return showTypeNm;
+	}
+
+	public void setShowTypeNm(String showTypeNm) {
+		this.showTypeNm = showTypeNm;
+	}
+
+	public String getAudits() {
+		return audits;
+	}
+
+	public void setAudits(String audits) {
+		this.audits = audits;
+	}
+
+	public String getAuditNo() {
+		return auditNo;
+	}
+
+	public void setAuditNo(String auditNo) {
+		this.auditNo = auditNo;
+	}
+
+	public String getWatchGradeNm() {
+		return watchGradeNm;
+	}
+
+	public void setWatchGradeNm(String watchGradeNm) {
+		this.watchGradeNm = watchGradeNm;
+	}
+
+	public String getCompanys() {
+		return companys;
+	}
+
+	public void setCompanys(String companys) {
+		this.companys = companys;
+	}
+
+	public String getCompanyCd() {
+		return companyCd;
+	}
+
+	public void setCompanyCd(String companyCd) {
+		this.companyCd = companyCd;
+	}
+
+	public String getCompanyNm() {
+		return companyNm;
+	}
+
+	public void setCompanyNm(String companyNm) {
+		this.companyNm = companyNm;
+	}
+
+	public String getCompanyNmEn() {
+		return companyNmEn;
+	}
+
+	public void setCompanyNmEn(String companyNmEn) {
+		this.companyNmEn = companyNmEn;
+	}
+
+	public String getCompanyPartNm() {
+		return companyPartNm;
+	}
+
+	public void setCompanyPartNm(String companyPartNm) {
+		this.companyPartNm = companyPartNm;
+	}
+
+	public String getStaffs_peopleNm() {
+		return staffs_peopleNm;
+	}
+
+	public void setStaffs_peopleNm(String staffs_peopleNm) {
+		this.staffs_peopleNm = staffs_peopleNm;
+	}
+
+	public String getStaffs_peopleNmEn() {
+		return staffs_peopleNmEn;
+	}
+
+	public void setStaffs_peopleNmEn(String staffs_peopleNmEn) {
+		this.staffs_peopleNmEn = staffs_peopleNmEn;
+	}
+
+	public String getStaffRoleNm() {
+		return staffRoleNm;
+	}
+
+	public void setStaffRoleNm(String staffRoleNm) {
+		this.staffRoleNm = staffRoleNm;
+	}
+}
---
+++ src/main/java/com/test/pro/DataColtroller.java
@@ -0,0 +1,134 @@
+package com.test.pro;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.data.vo.MovieVO;
+import com.data.vo.RankVO;
+
+@Controller
+public class DataColtroller {
+
+	@RequestMapping("/dd.inc")
+	public Map<String, Object> Dailydata() throws Exception{
+		
+		// 금일 박스오피스 순위
+		URL Dailyurl = new URL("http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml?key=4855fdf6db4ccb1111545e16fb5c682b&targetDt=20200317");
+		
+		Element root = connectXML(Dailyurl);
+		
+		Element dailyBoxOfficeList = root.getChild("dailyBoxOfficeList");
+		
+		List<Element> d_list = dailyBoxOfficeList.getChildren("dailyBoxOffice"); 
+		
+		RankVO[] ar = new RankVO[d_list.size()];
+		
+		int i=0;
+		for(Element e : d_list) {
+			RankVO vo = new RankVO();
+			
+			vo.setRnum(e.getChildText("rnum"));			// 순번
+			vo.setMovieCd(e.getChildText("movieCd"));	// 영화 코드
+			vo.setMovieNm(e.getChildText("movieNm"));	// 영화 이름
+			vo.setOpenDt(e.getChildText("openDt"));		// 개봉일
+			
+			//System.out.println(e.getChildText("movieNm"));
+			
+			ar[i++] = vo;
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("Dailyar", ar);
+		return map;
+	}
+	
+	public Map<String, Object> Weeklydata() throws Exception{
+		
+		// 금주 박스오피스 순위
+		URL Weeklyurl = new URL("http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.xml?key=4855fdf6db4ccb1111545e16fb5c682b&targetDt=20200310");
+		
+		Element root = connectXML(Weeklyurl);
+	
+		Element weeklyBoxOfficeList = root.getChild("weeklyBoxOfficeList");
+		
+		List<Element> w_list = weeklyBoxOfficeList.getChildren("weeklyBoxOffice"); 
+		
+		RankVO[] ar = new RankVO[w_list.size()];
+
+		int i=0;
+		for(Element e : w_list) {
+			RankVO vo = new RankVO();
+			
+			vo.setRnum(e.getChildText("rnum"));			// 순번
+			vo.setMovieCd(e.getChildText("movieCd"));	// 영화 코드
+			vo.setMovieNm(e.getChildText("movieNm"));	// 영화 이름
+			vo.setOpenDt(e.getChildText("openDt"));		// 개봉일
+
+			ar[i++] = vo;
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("Weeklyar", ar);
+		return map;
+	}
+	
+	
+	
+	// 미완성-----------------------------------------------------
+	public Map<String, Object> Moviedata(String movieCd) throws Exception{
+		
+		// 영화 상세정보
+		URL Weeklyurl = new URL("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.xml?key=4855fdf6db4ccb1111545e16fb5c682b&movieCd="+movieCd);
+		
+		Element root = connectXML(Weeklyurl);
+	
+		Element weeklyBoxOfficeList = root.getChild("weeklyBoxOfficeList");
+		
+		List<Element> w_list = weeklyBoxOfficeList.getChildren("weeklyBoxOffice"); 
+		
+		MovieVO[] ar = new MovieVO[w_list.size()];
+
+		int i=0;
+		for(Element e : w_list) {
+			MovieVO vo = new MovieVO();
+			
+
+			ar[i++] = vo;
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("moviInfo", ar);
+		return map;
+	}
+
+	public Element connectXML(URL url) throws Exception{
+		// 연결객체 생성
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		
+		// 연결객체로 부터 결과를 받는 contentType설정
+		conn.setRequestProperty("Content-Type", "application/xml");
+		
+		conn.connect();
+		
+		// 연결된 결과는 xml문서이고 그 문서를
+		// Document로 로드하기 위해 파서 준비
+		SAXBuilder builder = new SAXBuilder();
+
+		Document doc = builder.build(conn.getInputStream());
+		
+		// 루트 반환
+		return doc.getRootElement();
+	}
+}
